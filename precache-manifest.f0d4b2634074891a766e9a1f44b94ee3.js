88be733c5ba9e66270e888b512d9e9cf0b193afb
Louis Faucon <lpfaucon@gmail.com>
2019-08-22 22:07:40 +0200

    Updates

---
+++ precache-manifest.f0d4b2634074891a766e9a1f44b94ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eaa563a7ce6c4d23f252591f1a541fb",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "aa30a89b820c6f7f9d15",
+    "url": "/pomodoro/static/css/main.2302412f.chunk.css"
+  },
+  {
+    "revision": "4458b2fc10929ad2845e",
+    "url": "/pomodoro/static/js/2.b8c86260.chunk.js"
+  },
+  {
+    "revision": "aa30a89b820c6f7f9d15",
+    "url": "/pomodoro/static/js/main.6add1cbc.chunk.js"
+  },
+  {
+    "revision": "0cf99d9e6445e1215f2c",
+    "url": "/pomodoro/static/js/runtime~main.ad7a03aa.js"
+  }
+]);
