8d8c74f843cb00b14208f5c01b7caf33ac3f3f5b
Louis Faucon <lpfaucon@gmail.com>
2020-10-06 10:28:53 +0200

    Simplifying the interface and removing extraneous features

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 var INTERVAL;
+const MINUTE = 60000;
 
 const App = () => {
-  const [playing, setPlaying] = useState(false);
-  const [currentTime, setCurrentTime] = useState("00:00:00");
-  const [startTime, setSartTime] = useState(undefined);
-  const [completed, setCompleted] = useState(false);
+  const [display, setDisplay] = useState('Pomodoro');
+  const [step, setStep] = useState('stopped');
 
   const stop = () => {
-    setPlaying(false);
+    setStep('stopped');
     if (INTERVAL) clearInterval(INTERVAL);
-    setCurrentTime("00:00:00");
-    setSartTime(undefined);
+    setDisplay('Focus Time!');
   };
 
-  const play = target => {
-    setPlaying(true);
-    setCompleted(false);
+  const play = (target, callback) => {
     if (INTERVAL) clearInterval(INTERVAL);
-    const start = startTime || new Date();
-    setSartTime(start);
-    INTERVAL = setInterval(() => update(start, target), 42);
+    const start = new Date();
+    INTERVAL = setInterval(() => update(start, target, callback), 42);
   };
 
-  const update = (start, target) => {
-    const time = new Date() - start;
+  const work = () => {
+    setStep('working');
+    play(25, () => {
+      setStep('stopped');
+      setDisplay('Work Session Completed!');
+    });
+  };
+
+  const takeBreak = () => {
+    setStep('relaxed');
+    play(5, () => {
+      setStep('stopped');
+      setDisplay('Back to work');
+    });
+  };
+
+  const update = (start, target, callback) => {
+    const time = target * MINUTE - (new Date() - start);
     const minutes = Math.floor(time / 60000);
     const seconds = Math.floor(time / 1000) % 60;
     const centi = Math.floor(time / 10) % 100;
-    const d = x => (x < 10 ? "0" + x : "" + x);
-    setCurrentTime(d(minutes) + ":" + d(seconds) + ":" + d(centi));
-    if (time > target * 60000) {
-      setCompleted(true);
+    const d = (x) => (x < 10 ? '0' + x : '' + x);
+    setDisplay(d(minutes) + ':' + d(seconds) + ':' + d(centi));
+    if (time < 0) {
       stop();
-      document.getElementById("audio").play();
+      document.getElementById('audio').play();
+      callback();
     }
   };
 
@@ -44,32 +55,24 @@ const App = () => {
       <audio id="audio">
         <source src="powerUp6.mp3" type="audio/mpeg" />
       </audio>
-      <div className="timer">{completed ? "COMPLETE" : currentTime}</div>
-      {playing && (
+      <div className="timer">{display}</div>
+      {step === 'stopped' && (
+        <div className="button" onClick={work}>
+          WORK
+        </div>
+      )}
+      {step === 'stopped' && (
+        <div className="button" onClick={takeBreak}>
+          TAKE A BREAK
+        </div>
+      )}
+      {(step === 'relaxed' || step === 'working') && (
         <div className="button" onClick={stop}>
           STOP
         </div>
       )}
-      {!playing && (
-        <div className="button" onClick={() => play(25)}>
-          25 min
-        </div>
-      )}
-      {!playing && (
-        <div className="button" onClick={() => play(15)}>
-          15 min
-        </div>
-      )}
-      {!playing && (
-        <div className="button" onClick={() => play(5)}>
-          5 min
-        </div>
-      )}
-      {!playing && (
-        <div className="button" onClick={() => play(2)}>
-          2 min
-        </div>
-      )}
+      {step === 'working' && <div className="button">FOCUS!</div>}
+      {step === 'relaxed' && <div className="button">Enjoy Life :)</div>}
     </div>
   );
 };
